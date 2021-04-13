b138ae106489c488f9b03fa929c0ff6b723e29db
Sejin Kim <kim3@kenyon.edu>
2021-04-13 00:09:26 -0400

    Upgraded all of the monte carlo simulations to 100k epochs

--- R/fisher.R
+++ R/fisher.R
@@ -10,19 +10,19 @@ View(sca)
 
 # Run a fisher test without PKs
 sca_nopenalty <- filter(sca, ispenalty == 0)
-fisher.test(table(sca_nopenalty$team, sca_nopenalty$event), simulate.p.value = TRUE, B = 2000)
+fisher.test(table(sca_nopenalty$team, sca_nopenalty$event), simulate.p.value = TRUE, B = 100000)
 
 # Compare just manutd and colcrew
 sca_mancol <- filter(sca_nopenalty, team == 'manutd' | team == 'colcrew')
-fisher.test(table(sca_mancol$team, sca_mancol$event), simulate.p.value = TRUE)
+fisher.test(table(sca_mancol$team, sca_mancol$event), simulate.p.value = TRUE, B = 100000)
 
 # Compare just kenyon and colcrew
 sca_kencol <- filter(sca_nopenalty, team == 'kenyon' | team == 'colcrew')
-fisher.test(table(sca_kencol$team, sca_kencol$event), simulate.p.value = TRUE)
+fisher.test(table(sca_kencol$team, sca_kencol$event), simulate.p.value = TRUE, B = 100000)
 
 # Compare just kenyon and manutd
 sca_kenman <- filter(sca_nopenalty, team == 'kenyon' | team == 'manutd')
-fisher.test(table(sca_kenman$team, sca_kenman$event), simulate.p.value = TRUE)
+fisher.test(table(sca_kenman$team, sca_kenman$event), simulate.p.value = TRUE, B = 100000)
 
 # Run a fisher test on PKs
 fisher.test(table(sca$team, sca$ispenalty))
@@ -33,10 +33,10 @@ fisher.test(table(sca_nopenalty$team, sca_nopenalty$isassisted))
 # Just for curiosity
 # Compare just manutd and colcrew with PKs
 scapen_mancol <- filter(sca, team == 'manutd' | team == 'colcrew')
-fisher.test(table(scapen_mancol$team, scapen_mancol$event), simulate.p.value = TRUE)
+fisher.test(table(scapen_mancol$team, scapen_mancol$event), simulate.p.value = TRUE, B = 100000)
 # Compare just kenyon and colcrew
 scapen_kencol <- filter(sca, team == 'kenyon' | team == 'colcrew')
-fisher.test(table(scapen_kencol$team, scapen_kencol$event), simulate.p.value = TRUE)
+fisher.test(table(scapen_kencol$team, scapen_kencol$event), simulate.p.value = TRUE, B = 100000)
 # Compare just kenyon and manutd
 scapen_kenman <- filter(sca, team == 'kenyon' | team == 'manutd')
-fisher.test(table(scapen_kenman$team, scapen_kenman$event), simulate.p.value = TRUE)
+fisher.test(table(scapen_kenman$team, scapen_kenman$event), simulate.p.value = TRUE, B = 100000)
